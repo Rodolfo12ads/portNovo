edb48b3e5178a7e7f1db69d414aa0d4549a35a0d
Rodolfo12ads <126685464+Rodolfo12ads@users.noreply.github.com>
2025-01-13 20:37:28 -0300

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-})
+    baseURL: 'https://portfolio-6e8a.onrender.com', // Substitua pelo URL correto
+  });
+  
 
 //  depois de ter configurado o axi como esta é so 
 //  importar no index do projeto 
