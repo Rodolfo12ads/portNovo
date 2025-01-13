c2cadd36bb62e4f6d04e7ea8149fb9ff3a7580fa
Rodolfo12ads <126685464+Rodolfo12ads@users.noreply.github.com>
2025-01-13 19:57:16 -0300

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:10000",
+    baseURL: "http://localhost:3000",
 })
 
 //  depois de ter configurado o axi como esta é so 
